220370ebf29691f526b6b578508cdfe23128cce6
wendy <3063766545@qq.com>
2019-04-16 14:26:50 +0800

    实现路由功能

--- project/lib/router.js
+++ project/lib/router.js
@@ -10,3 +10,40 @@ let router = {
     }
 };
 
+/**
+ *添加路由的方法，method为请求方法，url为请求地址，callback为处理该请求的回调函数
+ *
+ * @param { String } method
+ * @param { String } url
+ * @param { Function } callback
+ */
+function addRouter(method, url, callback){
+    // 为了方便处理，将method和url统一转换成小写
+    method = method.toLowerCase();
+    url = url.toLowerCase();
+    // 将处理请求的回调函数，按方法名和地址存到路由表中
+    router[method][url] = callback;
+}
+
+/**
+ *查找处理请求的回调函数的方法，method为请求的方法，url为请求的地址，返回处理路由的回调函数
+ *
+ * @param { String } method
+ * @param { String } url
+ */
+function findRouter(method, url){
+    // 为了方便处理，将method和url统一转换成小写
+    method = method.toLowerCase();
+    url = url.toLowerCase();
+
+    // 找到路由的回调函数，不存在则默认返回null
+    const callback = router[method][url] || null;
+
+    // 将回调函数返回
+    return callback;
+}
+
+module.exports = {
+    addRouter,
+    findRouter
+}
